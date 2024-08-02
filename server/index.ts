14a96bcbe79129fab718acee850d15b4a859de85
Brian Smiley <briantsmiley42@gmail.com>
2024-08-02 17:10:10 -0400

    allows checking whole queue length and adds interval truncation/logging

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,6 @@ app.post("/api/click/:userId", async (req, res) => {
   const bufferCount = await exportedRedisClient.countClicksInTimeRange(userId);
   if (bufferCount >= 10) return res.status(429).json({ bufferCount });
   exportedRedisClient.addClick(now, userId);
-  console.log(`Fetched buffer count for user ${userId}: ${bufferCount}`);
   return res.status(200).json({ bufferCount });
 });
 app.get("/api/count/", async (req, res) => {
@@ -28,8 +27,16 @@ app.get("/api/buffer/:userId", async (req, res) => {
   res.json({ bufferCount });
 });
 //every 15 seconds, truncate the queue to the last 11 seconds
-const interval = setInterval(() => {
-  exportedRedisClient.truncateQueue(11000);
+const interval = setInterval(async () => {
+  //log the fac tthat we are truncating the queue, the current length of the queue, then log the new length after truncation
+  console.log("Truncating queue...");
+  const beforeLength = await exportedRedisClient.countClicksInTimeRange("", 0);
+  console.log(`Queue length before truncation: ${beforeLength}`);
+  const removed = await exportedRedisClient.truncateQueue(11000);
+  const afterLength = await exportedRedisClient.countClicksInTimeRange("");
+  console.log(
+    `Queue length after truncation: ${afterLength}; removed ${removed} items`
+  );
 }, 15000);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- server/redis.ts
+++ server/redis.ts
@@ -7,12 +7,12 @@ export const exportedRedisClient = {
     rawRedisClient.zadd("clicks", time.getTime(), value);
     rawRedisClient.incr("count");
   },
-  /**Returns the number of clicks from the given user in the time range */
+  /**Returns the number of clicks from the given user in the time range; passing 0 as milliseconds time range will return all clicks */
   countClicksInTimeRange: async (
     userId: string,
     milliseconds: number = 10000
   ) => {
-    const startTime = Date.now() - milliseconds;
+    const startTime = milliseconds === 0 ? "-inf" : Date.now() - milliseconds; //passing 0 as milliseconds will return all clicks
     const endTime = Date.now();
     const clicks = await rawRedisClient.zrangebyscore(
       "clicks",
@@ -25,9 +25,9 @@ export const exportedRedisClient = {
     const count = await rawRedisClient.get("count");
     return parseInt(count ?? "0");
   },
-  truncateQueue: (range: number = 10000) => {
+  truncateQueue: async (range: number = 10000) => {
     //remove all clicks older than the given range
     const startTime = Date.now() - range;
-    rawRedisClient.zremrangebyscore("clicks", "-inf", startTime);
+    return await rawRedisClient.zremrangebyscore("clicks", "-inf", startTime);
   }
 };
